cf84199e020c96c0c4d83b3d9b65a28d2294e1b2
ShiningDevStar <stunningpersonal@proton.me>
2023-02-15 17:16:56 -0800

    hi

--- smart-contract-verifier-http/src/main.rs
+++ smart-contract-verifier-http/src/main.rs
@@ -3,13 +3,12 @@ use smart_contract_verifier_http::{init_logs, run, Settings};
 use std::error::Error;
 
 use mongodb::{Client, options::{ClientOptions, ResolverConfig}};
-use std::env;
+use chrono::{TimeZone, Utc};
+use mongodb::bson::doc;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let settings = Settings::new().context("failed to parse config")?;
-    init_logs(settings.jaeger.clone());
-    run(settings).await?;
 
     // Load the MongoDB connection string from an environment variable:
    let client_uri =
@@ -27,6 +26,20 @@ async fn main() -> Result<(), Box<dyn Error>> {
    for name in client.list_database_names(None, None).await? {
       println!("- {}", name);
    }
+
+    let new_doc = doc! {
+      "title": "Parasite",
+      "year": 2020,
+      "plot": "A poor family, the Kims, con their way into becoming the servants of a rich family, the Parks. But their easy life gets complicated when their deception is threatened with exposure.",
+      "released": Utc.ymd(2020, 2, 7).and_hms(0, 0, 0),
+   };
+   // Get the 'movies' collection from the 'sample_mflix' database:
+   let movies = client.database("sample_mflix").collection("movies");
+   let insert_result = movies.insert_one(new_doc.clone(), None).await?;
+   println!("New document ID: {}", insert_result.inserted_id);
+
+   init_logs(settings.jaeger.clone());
+   run(settings).await?;
    
     Ok(())
 }
