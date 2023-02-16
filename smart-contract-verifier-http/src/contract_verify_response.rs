f444774dbc2ff00113dee524be765a5c060fff60
ShiningDevStar <stunningpersonal@proton.me>
2023-02-15 22:04:35 -0800

    1

--- smart-contract-verifier-http/src/contract_verify_response.rs
+++ smart-contract-verifier-http/src/contract_verify_response.rs
@@ -5,15 +5,15 @@ use serde::{Deserialize, Serialize};
 pub struct Contract_verify_response {
     pub file_name: String,
     pub contract_name: String,
-    compiler_version: String,
-    evm_version: String,
-    constructor_arguments: String,
-    optimization: bool,
-    optimization_runs: String,
-    contract_libraries: {},
-    abi: String,
-    sources: {},
-    compiler_settings: "",
-    local_creation_input_parts: [],
-    local_deployed_bytecode_parts: []
+    pub compiler_version: String,
+    pub evm_version: String,
+    pub constructor_arguments: String,
+    pub optimization: bool,
+    pub optimization_runs: String,
+    pub contract_libraries: {},
+    pub abi: String,
+    pub sources: {},
+    pub compiler_settings: "",
+    pub local_creation_input_parts: [],
+    pub local_deployed_bytecode_parts: []
 }
