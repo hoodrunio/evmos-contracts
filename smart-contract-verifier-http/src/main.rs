da519b7775021c9e3208fc3c2303711db378480d
ShiningDevStar <stunningpersonal@proton.me>
2023-02-15 18:39:42 -0800

    1

--- smart-contract-verifier-http/src/main.rs
+++ smart-contract-verifier-http/src/main.rs
@@ -5,7 +5,7 @@ use std::error::Error;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let settings = Settings::new().context("failed to parse config")?;
-   DB::new().db();
+   DB::new();
    init_logs(settings.jaeger.clone());
    run(settings).await?;
    
