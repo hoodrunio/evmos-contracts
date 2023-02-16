45b7aac60afb3323e0818fc9a45e539c308e4864
ShiningDevStar <stunningpersonal@proton.me>
2023-02-16 01:13:44 -0800

    1

--- smart-contract-verifier-http/src/main.rs
+++ smart-contract-verifier-http/src/main.rs
@@ -5,10 +5,6 @@ use std::error::Error;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
    let settings = Settings::new().context("failed to parse config")?;
-   //    // Get the 'movies' collection from the 'sample_mflix' database:
-//    let movies = client.database("sample_mflix").collection("movies");
-//    let insert_result = movies.insert_one(new_doc.clone(), None).await?;
-//    println!("New document ID: {}", insert_result.inserted_id);
    
    init_logs(settings.jaeger.clone());
    run(settings).await?;
