b60f44b14443fa7a1ba0f34c1eab5c6799f5f30a
ShiningDevStar <stunningpersonal@proton.me>
2023-02-19 07:43:14 -0800

    1

--- smart-contract-verifier-http/src/verified_contract_result.rs
+++ smart-contract-verifier-http/src/verified_contract_result.rs
@@ -3,6 +3,6 @@ use crate::verification_response::VerificationResult;
 
 #[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
 pub struct Verified_Contract_Result {
-    contract_address: String,
-    result: VerificationResult
+    pub contract_address: String,
+    pub result: VerificationResult
 }
