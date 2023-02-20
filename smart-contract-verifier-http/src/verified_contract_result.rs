f76b7777d09fabdd3a3b27e926fcf70d66883b41
ShiningDevStar <stunningpersonal@proton.me>
2023-02-20 11:17:44 -0800

    feat: add contract address to store

--- smart-contract-verifier-http/src/verified_contract_result.rs
+++ smart-contract-verifier-http/src/verified_contract_result.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 use crate::verification_response::VerificationResult;
 
+// struct to store contract verified result with contract address
 #[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
 pub struct Verified_Contract_Result {
     pub contract_address: String,
