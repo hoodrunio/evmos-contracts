f0f67065ae7bfea3d7d3195e2fc6b96c96040672
ShiningDevStar <stunningpersonal@proton.me>
2023-02-21 06:16:27 -0800

    fix: remove match

--- smart-contract-verifier-http/src/handlers/solidity_multi_part.rs
+++ smart-contract-verifier-http/src/handlers/solidity_multi_part.rs
@@ -28,7 +28,7 @@ pub struct MultiPartFiles {
 
 pub async fn get_Code() -> anyhow::Result<String, Err> {
     let rpc = Web3::new("https://evmos-evm.publicnode.com".to_string());
-    match rpc.eth_get_code("0xBbD37BF85f7474b5bDe689695674faB1888565Ad", None).await?
+    rpc.eth_get_code("0xBbD37BF85f7474b5bDe689695674faB1888565Ad", None).await?
 }
 
 #[instrument(skip(client, params), level = "debug")]
