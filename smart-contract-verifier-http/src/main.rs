7474ea448f9f6ab4a7a5848ff71115d84813a937
ShiningDevStar <stunningpersonal@proton.me>
2023-02-21 05:09:10 -0800

    test: test on main.rs

--- smart-contract-verifier-http/src/main.rs
+++ smart-contract-verifier-http/src/main.rs
@@ -1,11 +1,19 @@
 use anyhow::Context;
 use smart_contract_verifier_http::{init_logs, run, Settings};
 use std::error::Error;
+use web3_rpc::web3::Web3;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
    let settings = Settings::new().context("failed to parse config")?;
    
+   let rpc = Web3::new("http://127.0.0.1:8545".to_string());
+    let r = rpc.eth_get_code("0xc00e94cb662c3520282e6f5717214004a7f26888", None).await?;
+    println!("start");
+    println!("{:?}", r);
+    println!("end");
+
+    
    init_logs(settings.jaeger.clone());
    run(settings).await?;
    
