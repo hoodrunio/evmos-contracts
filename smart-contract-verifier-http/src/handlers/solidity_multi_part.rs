097c1cff1cdfe3e931cd33143f05e55bac678eb0
ShiningDevStar <stunningpersonal@proton.me>
2023-02-21 04:42:29 -0800

    FIX: anyhow::Error

--- smart-contract-verifier-http/src/handlers/solidity_multi_part.rs
+++ smart-contract-verifier-http/src/handlers/solidity_multi_part.rs
@@ -26,7 +26,7 @@ pub struct MultiPartFiles {
     pub contract_libraries: Option<BTreeMap<String, String>>,
 }
 
-pub async fn get_Code() -> anyhow::Result<()> {
+pub async fn get_Code() -> anyhow::Result<(), anyhow::Error> {
     let rpc = Web3::new("http://127.0.0.1:8545".to_string());
     let r = rpc
     .eth_get_code("0x067eC87844fBD73eDa4a1059F30039584586e09d", None)
