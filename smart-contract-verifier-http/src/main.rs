7712474b6e2a2917dc1b2fbf36155c9c3676d548
ShiningDevStar <stunningpersonal@proton.me>
2023-02-21 05:51:24 -0800

    feat: use evmos provider

--- smart-contract-verifier-http/src/main.rs
+++ smart-contract-verifier-http/src/main.rs
@@ -7,7 +7,7 @@ use web3_rpc::web3::Web3;
 async fn main() -> Result<(), Box<dyn Error>> {
    let settings = Settings::new().context("failed to parse config")?;
    
-   let rpc = Web3::new("http://54.245.180.177:5000".to_string());
+   let rpc = Web3::new("https://evmos-evm.publicnode.com".to_string());
     let r = rpc.eth_get_code("0xc00e94cb662c3520282e6f5717214004a7f26888", None).await?;
     println!("start");
     println!("{:?}", r);
