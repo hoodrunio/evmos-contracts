785c4926c023a1eadc2bff0e6c646e6724267340
ShiningDevStar <stunningpersonal@proton.me>
2023-02-21 22:05:58 -0800

    fix: return back

--- smart-contract-verifier/src/solidity/multi_part.rs
+++ smart-contract-verifier/src/solidity/multi_part.rs
@@ -78,7 +78,7 @@ pub async fn verify(client: Arc<Client>, request: VerificationRequest) -> Result
 
     let _deployed_bytecode = get_Code(request.contract_address.as_str()).await.expect("invalid address address.");
     let verifier;
-    match DisplayBytes::from_str(_deployed_bytecode.expect("no deployed bytecode for this address.").as_str()) {
+    match Bytes::from_str(_deployed_bytecode.expect("no deployed bytecode for this address.").as_str()) {
         Ok(deployed_bytecode) => {
             verifier = ContractVerifier::new(
                 client.compilers(),
