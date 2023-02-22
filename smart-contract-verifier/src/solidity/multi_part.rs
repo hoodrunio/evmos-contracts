74241623bfa9607f09ffde3dc60484082244a51f
ShiningDevStar <stunningpersonal@proton.me>
2023-02-21 22:19:59 -0800

    fix: add verifier

--- smart-contract-verifier/src/solidity/multi_part.rs
+++ smart-contract-verifier/src/solidity/multi_part.rs
@@ -77,20 +77,12 @@ pub async fn verify(client: Arc<Client>, request: VerificationRequest) -> Result
     let compiler_version = request.compiler_version;
 
     let _deployed_bytecode = get_Code(request.contract_address.as_str()).await.expect("invalid address address.");
-    let verifier;
-    match Bytes::from(_deployed_bytecode.expect("no deployed bytecode for this address.").as_str()) {
-        Some(deployed_bytecode) => {
-            verifier = ContractVerifier::new(
+    let verifier = ContractVerifier::new(
                 client.compilers(),
                 &compiler_version,
                 request.creation_bytecode,
-                deployed_bytecode,
+                Bytes::from(_deployed_bytecode.expect("no deployed bytecode for this address.").as_str()) 
             )?;
-        },
-        None => {
-            panic!("Invalid address")
-        }
-    };
     
     // println!("in solidity::multi_part::verify: {:?}", get_Code(request.contract_address.as_str()).await);
     // let deployed_bytecode = DisplayBytes::from_str(&value.deployed_bytecode)
